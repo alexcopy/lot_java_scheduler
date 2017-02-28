ff54a6bffdc91e983d10c9e79e3774fe81893164
Alex Redko <alexr@nr-online.com>
2017-02-28 12:54:53 +0000

    Fix comma in WeekDay enum

--- src/main/java/ru/m2mcom/natmob/domain/enumeration/WeekDay.java
+++ src/main/java/ru/m2mcom/natmob/domain/enumeration/WeekDay.java
@@ -4,5 +4,5 @@ package ru.m2mcom.natmob.domain.enumeration;
  * The WeekDay enumeration.
  */
 public enum WeekDay {
-    Sunday, Monday, Tuesday, Wednesday, Thursday, Friday Saturday
+    Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
 }
